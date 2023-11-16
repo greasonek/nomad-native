9dfea0cf2216ada01feba66d055187b300a50139
EMILY GREASON <GREASONEMILY@GMAIL.COM>
2023-11-15 21:48:27 -0800

    added font styling to home page

--- app/App.js
+++ app/App.js
@@ -1,21 +1,22 @@
 import { ImageBackground, StyleSheet, Text, View } from 'react-native';
-
+import { AmaticSC_700Bold } from "@expo-google-fonts/amatic-sc";
 const image = { uri: "https://images.unsplash.com/photo-1548932134-3d7d765bece2?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"};
-// import { useFonts } from "expo-font";
+import { useFonts } from "expo-font";
 
 export default function App() {
-  // const [fontsLoaded] = useFonts({
-  //   ZenLoop: require('../assets/ZenLoop-Regular.ttf'),
-  // });
+  const [fontsLoaded] = useFonts({
+    AmaticSC_700Bold,
+  });
 
-  // if(!fontsLoaded){
-  //   return null;
-  // }
+  if(!fontsLoaded){
+    return <Text>Loading...</Text>;
+  }
 
   return (
     <View style={styles.container}>
       <ImageBackground source={image} style={styles.image}>
-        <Text style={styles.text}>NOMAD</Text>
+        <Text style={styles.titleText}>NOMAD</Text>
+        <Text style={styles.buttonText}>Explore</Text>
       </ImageBackground>
     </View>
   );
@@ -31,13 +32,18 @@ const styles = StyleSheet.create({
     resizeMode: 'cover',
     justifyContent: 'center',
   },
-  text: {
+  titleText: {
     color: 'ivory',
-    fontSize: 82,
+    fontSize: 152,
     fontWeight: 'bold',
     textAlign: 'center',
-    fontFamily: 'AmaticSC-Regular',
+    fontFamily: 'AmaticSC_700Bold',
   },
-  fontText: {
+  buttonText: {
+    color: 'ivory',
+    fontSize: 32,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    fontFamily: 'AmaticSC_700Bold',
   }
 });
